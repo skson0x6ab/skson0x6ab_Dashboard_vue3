9e8e24daf7c6c46e77334247e74c36f3a53f9aba
skson0x6ab <skson0x6ab@gmail.com>
2024-12-16 17:43:00 +0900

    [PN] Update favicon

--- src/partials/dashboard/ZenlessCodeRedeemDashboard.vue
+++ src/partials/dashboard/ZenlessCodeRedeemDashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-span-full xl:col-span-8 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
     <header class="px-5 py-4 border-b border-gray-100 dark:border-gray-700/60">
-      <h2 class="font-semibold text-gray-800 dark:text-gray-100">CodeRedeem: Genshin Impact</h2>
+      <h2 class="font-semibold text-gray-800 dark:text-gray-100">CodeRedeem: Zenless Zone Zero</h2>
     </header>
     <div class="p-3">
       <!-- 로딩 중 상태 -->
