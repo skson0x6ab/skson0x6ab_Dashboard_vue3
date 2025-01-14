5fd3474b091c2d69268a1880097b990e7e63a40e
skson0x6ab <skson0x6ab@gmail.com>
2025-01-14 17:13:23 +0900

    미사용 패키지 삭제 및 vercel Analytics 를 위한 패키지 추가

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import router from './router'
 import App from './App.vue'
-
+import { inject } from '@vercel/analytics'
 
 import './css/style.css'
+
+
+inject()
 
 const app = createApp(App)
 app.use(createPinia());
